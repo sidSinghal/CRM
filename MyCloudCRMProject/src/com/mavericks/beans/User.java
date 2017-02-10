41edc39127957bcca48fcfa55fa76d64a33f2777
Sid <singhal.si@husky.neu.edu>
2017-02-10 11:02:58 -0500

    MyCloudCRMProject

---
+++ MyCloudCRMProject/src/com/mavericks/beans/User.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.mavericks.beans;
+
+/**
+ * @author sid
+ *
+ */
+public class User {
+
+	private String userName;
+	private String password;
+	
+	public User() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ MyCloudCRMProject/src/com/mavericks/tests/LoginTest.java
@@ -0,0 +1,15 @@
+package com.mavericks.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+		
+	}
+
+}
