0e34f53c66aa7c84591a78bfc53b5a66f1823a84
Siddhant <singhal.si@husky.neu.edu>
2017-03-24 00:48:49 -0400

    Adding test cases

---
+++ Email.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dell
+ * Date: 3/23/2017
+ * Time: 11:53 PM
+ */
+class Email
+{
+    private $email;
+
+    private function __construct($email)
+    {
+        $this->ensureIsValidEmail($email);
+
+        $this->email = $email;
+    }
+
+    public static function fromString($email)
+    {
+        return new self($email);
+    }
+
+    public function __toString()
+    {
+        return $this->email;
+    }
+
+    private function ensureIsValidEmail($email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid email address',
+                    $email
+                )
+            );
+        }
+    }
+
+}
