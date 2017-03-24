7d604537c6bae40cceda2eda5c8adb5410744afc
sneha-malshetti <malshetti.s@husky.neu.edu>
2017-03-24 01:14:01 -0400

    not required

--- CRMMavericksProject/src/main/java/com/mavericks/services/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.mavericks.services;
-
-public class LoginService {
-	public boolean validateUser(String user, String password) {
-		return user.equalsIgnoreCase("user123") && password.equals("password123");
-	}
-
-}
