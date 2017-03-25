f08da0ac2da2aee97b3695192b92d3252d42fe9e
DasRaseswari <das.r@husky.neu.edu>
2017-03-25 01:25:39 -0400

    Adding scripts to run the application

---
+++ scripts/change_permissions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod -R 755 /var/www/html/CRM
+chmod +x /tmp/CRM/scripts/*
---
+++ scripts/restartTomcat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo service tomcat7 stop
+cd /var/lib/tomcat7/webapps
+sudo rm -rf test-1.0.0
+sudo service tomcat7 start
